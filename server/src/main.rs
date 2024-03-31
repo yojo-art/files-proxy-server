6d43fe4846efedf9e998eaa3032850e857ce7fad
Kozakura913 <98575220+kozakura913@users.noreply.github.com>
2024-03-31 17:30:46 +0900

    要求してきたpathを返すだけのHTTPサーバー

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,14 @@
+use std::net::SocketAddr;
+
+use axum::{response::IntoResponse, routing::get, Router};
+
 fn main() {
-    println!("Hello, world!");
+	let addr = SocketAddr::from(([0, 0, 0, 0],8080));
+	tokio::runtime::Builder::new_multi_thread().enable_all().build().unwrap().block_on(async {
+		let app = Router::new().route("/*path",get(|axum::extract::Path(path):axum::extract::Path<String>|async{
+			println!("{}",path);
+			(axum::http::StatusCode::OK,path).into_response()
+		}));
+		axum::Server::bind(&addr).serve(app.into_make_service_with_connect_info::<SocketAddr>()).await.unwrap();
+	});
 }
